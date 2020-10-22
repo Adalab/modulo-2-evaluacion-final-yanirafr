9c28cc43bdc12577970cd7ad30e72ae489c6fb1e
Yanira <yanirafr@gmail.com>
2020-10-22 19:28:15 +0200

    add fetch function and movie list display from input

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const movieList = document.querySelector(".js-list");
+const btn = document.querySelector(".js-btn");
+
+function getMovie() {
+  const input = document.querySelector(".js-input").value;
+  fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data[0].show.name);
+      for (let i = 0; i < data.length; i++) {
+        movieList.innerHTML += `<li class="js-movie${i}"><img src="${data[i].show.image.medium}" alt="${data[i].show.name}" title="${data[i].show.name}">${data[i].show.name}</li>`;
+        // console.log(data[i].show.name);
+      }
+      //   addListItem();
+    });
+  console.log(input);
+}
+
+btn.addEventListener("click", getMovie);
+
+function addListItem() {}
