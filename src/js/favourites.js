2f290f8e5099cf6aa4f173b27d429fee64ab386f
Yanira <yanirafr@gmail.com>
2020-10-23 09:13:45 +0200

    add fumction for adding element to favourite list

--- src/js/favourites.js
+++ src/js/favourites.js
@@ -1,17 +1,39 @@
 "use strict";
 
+// Movie Arrays
 let browsedMovies = [];
 const favMovies = [];
-const favList = document.querySelector(".favourite__list");
+
+// Query selectors
+const favList = document.querySelector(".js-fav");
 const movieList = document.querySelector(".js-list");
 
+// Select Favourites
 function selectFavourite(ev) {
-  ev.currentTarget.classList.add("selected");
+  const favMoviesIndex = favMovies.indexOf(ev.currentTarget);
+  console.log(ev.currentTarget.innerHTML);
+  if (ev.currentTarget.classList.contains("selected")) {
+    ev.currentTarget.classList.remove("selected");
+    favMovies.splice(favMoviesIndex, 1);
+  } else {
+    ev.currentTarget.classList.add("selected");
+    favMovies.push(ev.currentTarget.innerHTML);
+  }
+  addFavourite();
 }
 
+// Movies event listener
 function addListener() {
   const movies = document.querySelectorAll(".js-movie");
   for (const movie of movies) {
     movie.addEventListener("click", selectFavourite);
+  }
+}
+
+// Add favourites to list
+function addFavourite() {
+  for (const movie of favMovies) {
+    favList.innerHTML = `<li>${movie}<li>`;
+    console.log("for", movie);
   }
 }
