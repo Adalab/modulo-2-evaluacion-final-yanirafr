a4952a4716cee5cb51ff309664981348c1ec9785
Yanira <yanirafr@gmail.com>
2020-10-25 00:35:59 +0200

    clean code

--- src/js/favourites.js
+++ src/js/favourites.js
@@ -43,17 +43,14 @@ function addListener() {
 
 // Add favourites to list
 function addFavourite() {
-  // let favMovieList = "";
   for (let i = 0; i < favMovies.length; i++) {
     //List element
-    // favMovieList += `<li class="js-fav-item js-fav${i} favourite__list__item">`;
     const favMovieList = document.createElement("li");
     favMovieList.classList.add("js-fav-item");
     favMovieList.classList.add(`js-fav${i}`);
     favMovieList.classList.add("favourite__list__item");
     
     // Add image
-    // favMovieList += `<img class="js-fav-img${i} favourite__list__img" `;
     // favMovieList += `alt="${favMovies[i].show.name}" title="${favMovies[i].show.name}">`;
     const listImg = document.createElement("img");
     favMovieList.appendChild(listImg);
@@ -63,15 +60,12 @@ function addFavourite() {
     listImg.setAttribute("title", `${favMovies[i].show.name}`);
 
     if (favMovies[i].show.image === null) {
-      // favMovieList += `src= "https://via.placeholder.com/210x295/ffffff/666666/?text=Image"`;
       listImg.src = "https://via.placeholder.com/210x295/ffffff/666666/?text=Image";
     } else {
-      // favMovieList += `src="${favMovies[i].show.image.medium}"`;
       listImg.src = `${favMovies[i].show.image.medium}`;
     }
 
     // Title
-    // favMovieList += `<h3 class="favourite__list__title">${favMovies[i].show.name}</h3>`;
     const favName = document.createElement("h3");
     favMovieList.appendChild(favName);
     favName.classList.add("favourite__list__title");
@@ -79,7 +73,6 @@ function addFavourite() {
     favName.appendChild(favNameText);
 
     // Button
-    // favMovieList += `<button class="favourite__list__btn js-delete-btn" id=${favMovies[i].show.id} title="Borrar">`;
     const favBtn = document.createElement("button");
     favMovieList.appendChild(favBtn);
     favBtn.setAttribute("id", `${favMovies[i].show.id}`);
@@ -88,18 +81,13 @@ function addFavourite() {
     favBtn.setAttribute("title", "Borrar");
 
     // Favicon
-    // favMovieList += `<i class="favourite__list__fav fas fa-trash-alt"></i>`;
     const favBtnFav = document.createElement("i");
     favBtn.appendChild(favBtnFav);
     favBtnFav.classList.add("favourite__list__fav");
     favBtnFav.classList.add("fas");
     favBtnFav.classList.add("fa-trash-alt");
-    
-    // favMovieList += `</button>`;
-    // favMovieList += `<li>`;
     favList.appendChild(favMovieList);
   }
-  // favList.innerHTML = favMovieList;
 
   addRemoveListener();
 }
