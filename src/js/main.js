73deff88993dc613252e2039b3bc68c47e04173a
Yanira <yanirafr@gmail.com>
2020-10-25 11:12:22 +0100

    cleanup code

--- src/js/main.js
+++ src/js/main.js
@@ -27,10 +27,6 @@ function addListItem() {
   for (let i = 0; i < browsedMovies.length; i++) {
 
     // List item
-    // let movie = "";
-    // movie += `<li title="${browsedMovies[i].show.name}"`;
-    // movie += `class="`;
-    // movie += `js-movie js-movie${i} movie__list__item" id="${browsedMovies[i].show.id}">`;
     const movie = document.createElement("li");
     movie.setAttribute("title", `${browsedMovies[i].show.name}`);
     movie.setAttribute("id", `${browsedMovies[i].show.id}`);
@@ -45,18 +41,9 @@ function addListItem() {
     );
     // console.log(foundFav);
     if (foundFav !== undefined) {
-      // movie += ``;
-      // movie.classList.add("a");
-      // console.log("en el if");
       movie.classList.add("selected");}
-    // } else {
-      // movie += `selected `;
-      // console.log("en el else");
-    // }
 
     //Image
-    // movie += `<img class="js-movie-img${i} movie__list__img" `;
-    // movie += ` alt="${browsedMovies[i].show.name}" title="${browsedMovies[i].show.name}">`;
     const movieImg = document.createElement("img");
     movie.appendChild(movieImg);
     movieImg.classList.add(`js-movie-img${i}`);
@@ -66,15 +53,12 @@ function addListItem() {
 
     // Add image src
     if (browsedMovies[i].show.image === null) {
-      // movie += `src= "https://via.placeholder.com/210x295/ffffff/666666/?text=Image"`;
       movieImg.src = "https://via.placeholder.com/210x295/ffffff/666666/?text=Image";
     } else {
-      // movie += `src = ${browsedMovies[i].show.image.medium}`;
       movieImg.src = `${browsedMovies[i].show.image.medium}`;
     }
 
     // Title
-    // movie += `<h3 class="movie__list__name">${browsedMovies[i].show.name}</h3>`;
     const movieTitle = document.createElement("h3");
     movie.appendChild(movieTitle);
     movieTitle.classList.add("movie__list__name");
@@ -82,8 +66,6 @@ function addListItem() {
     movieTitle.appendChild(movieTitleText);
     
     //Add to list
-    // movie += `</li>`;
-    // movieList.innerHTML += movie;
     movieList.appendChild(movie);
   }
 }
